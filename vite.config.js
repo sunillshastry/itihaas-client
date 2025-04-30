c9616b21d66c871676b2d0adb30d8466ba0dfeac
sunil <sunil.shastry69@gmail.com>
2025-04-30 10:12:45 -0600

    Initialize and setup configuration for Tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), ViteMinifyPlugin()],
+	plugins: [react(), tailwindcss(), ViteMinifyPlugin()],
 	server: {
 		host: '0.0.0.0',
 		port: 3000,
