762fc85e8f45479896e315cd615fe9f3f34d642c
sunil <sunil.shastry69@gmail.com>
2025-05-09 21:31:50 -0600

    Add code and logic for SearchAutocompleteTab to display new/relevant results after backend fetch

--- src/components/SearchAutocompleteTab.jsx
+++ src/components/SearchAutocompleteTab.jsx
@@ -1,41 +1,56 @@
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import NoResultsText from './NoResultsText';
+import AutocompleteLink from './AutocompleteLink';
 
-function SearchAutocompleteTab({ displayed, query }) {
+function SearchAutocompleteTab({ displayed, query, handleLinkClick }) {
+	const [queriedResults, setQueriedResults] = useState([]);
+
+	useEffect(
+		function () {
+			async function fetchAutocompleteOptions() {
+				// Fetch dynasties
+				const BASE_URL = import.meta.env.VITE_BASE_SERVER_URI;
+				const response = await fetch(
+					`${BASE_URL}/dynasties/search/titles?include=id,slug`
+				);
+				const data = await response.json();
+
+				// Fetch rulers
+				// Fetch wars
+
+				// Add to query list
+				if (response.ok && data?.success) {
+					setQueriedResults(
+						data.data.dynasties.filter((dynasty) =>
+							dynasty.name.toLowerCase().includes(query.toLowerCase())
+						)
+					);
+				} else {
+					setQueriedResults([]);
+				}
+			}
+			fetchAutocompleteOptions();
+		},
+		[query]
+	);
+
 	return (
 		<div
 			className={`absolute max-h-[250px] w-full overflow-y-scroll bg-white p-2 text-sm ${displayed ? 'block' : 'hidden'}`}
 		>
+			{queriedResults.length === 0 && <NoResultsText query={query} />}
+
 			<ul className="list-none">
-				<li className="autocomplete__link">
-					<a href="#">Link-1</a>
-				</li>
-				<li className="autocomplete__link">
-					<a href="#">Link-2</a>
-				</li>
-				<li className="autocomplete__link">
-					<a href="#">Link-3</a>
-				</li>
-				<li className="autocomplete__link">
-					<a href="#">Link-4</a>
-				</li>
-				<li className="autocomplete__link">
-					<a href="#">Link-5</a>
-				</li>
-				<li className="autocomplete__link">
-					<a href="#">Link-6</a>
-				</li>
-				<li className="autocomplete__link">
-					<a href="#">Link-7</a>
-				</li>
-				<li className="autocomplete__link">
-					<a href="#">Link-8</a>
-				</li>
-				<li className="autocomplete__link">
-					<a href="#">Link-9</a>
-				</li>
-				<li className="autocomplete__link">
-					<a href="#">Link-10</a>
-				</li>
+				{queriedResults.map(function (queriedResult) {
+					return (
+						<AutocompleteLink
+							key={queriedResult._id}
+							queryResult={queriedResult}
+							handleLinkClick={handleLinkClick}
+						/>
+					);
+				})}
 			</ul>
 		</div>
 	);
@@ -44,6 +59,7 @@ function SearchAutocompleteTab({ displayed, query }) {
 SearchAutocompleteTab.propTypes = {
 	displayed: PropTypes.bool.isRequired,
 	query: PropTypes.string,
+	handleLinkClick: PropTypes.func,
 };
 
 export default SearchAutocompleteTab;
