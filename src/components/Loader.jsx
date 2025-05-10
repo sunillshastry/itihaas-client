c240b02e71989a4bb7bd0668028f1dca823f5a1e
sunil <sunil.shastry69@gmail.com>
2025-05-10 01:47:26 -0600

    Update styling conditional check to something simpler and easy to read

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,28 +2,31 @@ import PropTypes from 'prop-types';
 
 function Loader({ size = 'medium' }) {
 	const styles = {
-		border:
-			(size === 'small' && '5px solid #b4957e') ||
-			(size === 'medium' && '8px solid #b4957e') ||
-			(size === 'large' && '12px solid #b4957e'),
-
-		borderTop:
-			(size === 'small' && '5px solid #4a3423') ||
-			(size === 'medium' && '8px solid #4a3423') ||
-			(size === 'large' && '12px solid #4a3423'),
-
+		border: '8px solid #b4957e',
+		borderTop: '8px solid #4a3423',
 		borderRadius: '50%',
-		width:
-			(size === 'small' && '25px') ||
-			(size === 'medium' && '50px') ||
-			(size === 'large' && '75px'),
-		height:
-			(size === 'small' && '25px') ||
-			(size === 'medium' && '50px') ||
-			(size === 'large' && '75px'),
+		width: '50px',
+		height: '50px',
 		animation: 'spin 1s linear infinite',
 		margin: '0 auto',
 	};
+
+	if (size === 'small') {
+		styles.border = '5px solid #b4957e';
+		styles.borderTop = '5px solid #4a3423';
+		styles.width = '25px';
+		styles.height = '25px';
+	} else if (size === 'medium') {
+		styles.border = '8px solid #b4957e';
+		styles.borderTop = '8px solid #4a3423';
+		styles.width = '50px';
+		styles.height = '50px';
+	} else if (size === 'large') {
+		styles.border = '12px solid #b4957e';
+		styles.borderTop = '12px solid #4a3423';
+		styles.width = '75px';
+		styles.height = '75px';
+	}
 
 	return <div style={styles}></div>;
 }
