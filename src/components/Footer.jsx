d56221fae1c5c3dd88aaa08653a1b1f3efb055bb
sunil <sunil.shastry69@gmail.com>
2025-05-11 06:32:54 -0600

    Build the footer section of the website under the Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,52 @@
+import FooterLinksTab from './FooterLinksTab';
+import FooterLink from './FooterLink';
+
 function Footer() {
+	const year = new Date().getFullYear();
+
 	return (
-		<div>
-			<h1>Footer</h1>
-		</div>
+		<footer className="bg-primary-600 flex justify-between px-14 py-6 pb-8">
+			<section className="w-[60%]">
+				<div>
+					<h3 className="font-logo text-primary-90 text-3xl">Itihaas</h3>
+					<p className="text-primary-10 text-sm">
+						&copy; {year} Itihaas | Licensed under the{' '}
+						<a
+							href="#"
+							target="_blank"
+							className="underline"
+						>
+							GNU GPL v3
+						</a>
+					</p>
+				</div>
+
+				<p className="text-primary-50 mt-4 leading-8">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Id tenetur
+					fugiat iure nesciunt, corrupti itaque quasi dolor reiciendis dolorem
+					nemo eligendi quisquam quos sapiente. Accusamus explicabo similique
+					repudiandae deleniti et praesentium harum, alias dolore ipsam!
+				</p>
+			</section>
+
+			<section className="flex w-[40%] items-start justify-around">
+				<FooterLinksTab title="Product">
+					<ul>
+						<FooterLink to="#">Dynasties</FooterLink>
+						<FooterLink to="#">Rulers</FooterLink>
+						<FooterLink to="#">Wars</FooterLink>
+					</ul>
+				</FooterLinksTab>
+
+				<FooterLinksTab title="Developer">
+					<ul>
+						<FooterLink to="#">Issues</FooterLink>
+						<FooterLink to="#">Docs</FooterLink>
+						<FooterLink to="#">GitHub</FooterLink>
+					</ul>
+				</FooterLinksTab>
+			</section>
+		</footer>
 	);
 }
 
