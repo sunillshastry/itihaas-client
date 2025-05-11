7b6c6812c1d77a8683933c056bcd7a15401af0d6
sunil <sunil.shastry69@gmail.com>
2025-05-11 08:43:55 -0600

    Replace 'underline' styling with border styling with updated colour/opacity

--- src/components/FooterLinksTab.jsx
+++ src/components/FooterLinksTab.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 function FooterLinksTab({ title, children }) {
 	return (
 		<div>
-			<h4 className="font-heading text-primary-60 text-xl underline underline-offset-3">
+			<h4 className="font-heading text-primary-60 border-b-primary-60/20 border-b text-xl">
 				{title}
 			</h4>
 			{children}
