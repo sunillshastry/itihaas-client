1d4b1744ff3b8e29519544ec60bcd726e7cd1078
sunil <sunil.shastry69@gmail.com>
2025-05-12 03:14:15 -0600

    Develop PageLinkField component file with native 'a' tag and Link component tag for page links

---
+++ src/components/PageLinkField.jsx
@@ -0,0 +1,42 @@
+import { SquareArrowOutUpRight } from 'lucide-react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+function PageLinkField({ native = false, children, className, to }) {
+	if (native) {
+		return (
+			<a
+				href={to}
+				target="_blank"
+				className={`text-primary-400 hover:text-primary-10 flex items-center underline ${className}`}
+			>
+				<span>{children}</span>
+				<span className="ml-1">
+					<SquareArrowOutUpRight size={15} />
+				</span>
+			</a>
+		);
+	}
+
+	return (
+		<Link
+			to={to}
+			className={`text-primary-400 hover:text-primary-10 underline ${className}`}
+		>
+			{children}
+		</Link>
+	);
+}
+
+PageLinkField.propTypes = {
+	native: PropTypes.boolean,
+	children: PropTypes.oneOfType([
+		PropTypes.element,
+		PropTypes.node,
+		PropTypes.string,
+	]),
+	className: PropTypes.string,
+	to: PropTypes.string.isRequired,
+};
+
+export default PageLinkField;
