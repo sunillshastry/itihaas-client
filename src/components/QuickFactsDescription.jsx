de1a44f7e0f8eb770a4e74d3b57a3403633f4da0
sunil <sunil.shastry69@gmail.com>
2025-05-12 02:56:03 -0600

    Update component file name - remove 'QuickFacts'

--- src/components/QuickFactsDescription.jsx
+++ src/components/QuickFactsDescription.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 
-function QuickFactsDescription({ description }) {
+function Description({ description }) {
 	return (
 		<p className="text-primary-400 text-md my-5 leading-10">{description}</p>
 	);
 }
 
-QuickFactsDescription.propTypes = {
+Description.propTypes = {
 	description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
 };
-export default QuickFactsDescription;
+export default Description;
