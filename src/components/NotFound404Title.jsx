01016515308b14e48635db1baa047178175648a1
sunil <sunil.shastry69@gmail.com>
2025-05-12 18:49:25 -0600

    Design NotFound404Title component to serve as the giant 404 title on NotFound page

---
+++ src/components/NotFound404Title.jsx
@@ -0,0 +1,9 @@
+function NotFound404Title() {
+	return (
+		<h2 className="text-primary-400/10 selection:text-primary-400/10 pointer-events-none absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform text-[450px] font-black selection:bg-transparent">
+			404
+		</h2>
+	);
+}
+
+export default NotFound404Title;
