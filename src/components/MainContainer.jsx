7be755616107934d7d1d0f1682544f6a6fde2ba9
sunil <sunil.shastry69@gmail.com>
2025-05-12 18:31:43 -0600

    Include 'className' prop to add additional custom styles as props

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 
-function MainContainer({ children }) {
-	return <main className="mx-36 mt-5">{children}</main>;
+function MainContainer({ children, className }) {
+	return <main className={`mx-36 mt-5 ${className}`}>{children}</main>;
 }
 
 MainContainer.propTypes = {
 	children: PropTypes.oneOfType([PropTypes.element, PropTypes.node]),
+	className: PropTypes.string,
 };
 
 export default MainContainer;
