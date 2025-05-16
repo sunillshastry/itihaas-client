f7c57c119babfb8eb6c3629d65c5f1d53006d09d
sunil <sunil.shastry69@gmail.com>
2025-05-16 16:51:49 -0600

    Build single Article component with styling and logic

---
+++ src/components/Article.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import formatArrayToString from '../utils/formatArrayToString';
+
+function Article({ title, link, publisher, authors = [] }) {
+	return (
+		<li className="bg-primary-60 shadow-primary my-4 rounded-sm px-3 py-2 shadow-xs transition-all duration-150 ease-in hover:shadow-md">
+			<a
+				href={link}
+				target="_blank"
+			>
+				<div>
+					<h4 className="text-primary-400 text-md mb-2 font-medium underline underline-offset-2">
+						{title}
+					</h4>
+					{authors.length > 0 ? (
+						<p className="text-primary text-sm">
+							By {formatArrayToString(authors)}
+						</p>
+					) : (
+						<p className="text-primary text-sm">Author unavailable</p>
+					)}
+
+					{publisher ? (
+						<p className="text-primary text-sm">Published by {publisher}</p>
+					) : (
+						<p className="text-primary text-sm">Publisher unavailable</p>
+					)}
+
+					<a
+						className="text-primary-300 mt-2 underline"
+						href={link}
+						target="_blank"
+					>
+						{link}
+					</a>
+				</div>
+			</a>
+		</li>
+	);
+}
+
+Article.propTypes = {
+	title: PropTypes.string,
+	authors: PropTypes.array,
+	link: PropTypes.string,
+	publisher: PropTypes.string,
+};
+
+export default Article;
