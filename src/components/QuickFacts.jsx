f57f9ae6ccc168487bfd7704612f2aba2ceaf976
sunil <sunil.shastry69@gmail.com>
2025-05-21 18:54:16 -0600

    Update QuickFacts component content to make it more reusable with other entities such as rulers, wars

--- src/components/QuickFacts.jsx
+++ src/components/QuickFacts.jsx
@@ -1,22 +1,23 @@
 import PropTypes from 'prop-types';
-import DynastyQuickFieldsContainer from './DynastyQuickFieldsContainer';
 
-function QuickFacts({ dynasty }) {
+function QuickFacts({ children }) {
 	return (
 		<section className="from-primary-100 via-primary-300 to-primary-500 mt-6 rounded-lg bg-linear-to-br px-5 py-8 shadow-lg">
 			<h2 className="text-primary-70 font-heading border-primary-70/35 border-b pb-3 text-2xl">
 				Quick Facts
 			</h2>
 
-			<ul className="mt-5 grid grid-cols-3 grid-rows-3 gap-y-5">
-				<DynastyQuickFieldsContainer dynasty={dynasty} />
-			</ul>
+			<ul className="mt-5 grid grid-cols-3 grid-rows-3 gap-y-5">{children}</ul>
 		</section>
 	);
 }
 
 QuickFacts.propTypes = {
-	dynasty: PropTypes.object.isRequired,
+	children: PropTypes.oneOfType([
+		PropTypes.element,
+		PropTypes.node,
+		PropTypes.string,
+	]),
 };
 
 export default QuickFacts;
