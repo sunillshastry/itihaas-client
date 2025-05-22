235ba61b1de5768354ccca3a6d920b181919c575
sunil <sunil.shastry69@gmail.com>
2025-05-21 19:01:25 -0600

    Implement user-interface for RulerPage page component: currently building on boilerplate data without any backend requests

---
+++ src/components/RulerQuickFieldsContainer.jsx
@@ -0,0 +1,40 @@
+import QuickFactField from './QuickFactField';
+
+function RulerQuickFieldsContainer() {
+	return (
+		<>
+			<QuickFactField
+				title="dynasty"
+				content="Kuru Dynasty"
+			/>
+
+			<QuickFactField
+				title="predecessor"
+				content="Vichitravirya"
+			/>
+
+			<QuickFactField
+				title="family"
+				content="Update this"
+			/>
+			<QuickFactField
+				title="religion"
+				content="Hinduism"
+			/>
+			<QuickFactField
+				title="successor"
+				content="Dhritarashtra II"
+			/>
+			<QuickFactField
+				title="reign"
+				content="-"
+			/>
+			<QuickFactField
+				title="reign end"
+				content="-"
+			/>
+		</>
+	);
+}
+
+export default RulerQuickFieldsContainer;
