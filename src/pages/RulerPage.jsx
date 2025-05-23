d1acd9006d464e9917bf576c18f14efbbf9de699
sunil <sunil.shastry69@gmail.com>
2025-05-23 16:32:58 -0600

    Implement functionality to fetch ruler data for the ruler page component. Currently includes just the fetch and no user-interface matchup

--- src/pages/RulerPage.jsx
+++ src/pages/RulerPage.jsx
@@ -1,12 +1,75 @@
+import { useEffect, useState } from 'react';
 import BackButton from '../components/BackButton';
+import DescriptionContainer from '../components/DescriptionContainer';
 import MainContainer from '../components/MainContainer';
 import Navbar from '../components/Navbar';
 import PrimaryHeader from '../components/PrimaryHeader';
 import QuickFacts from '../components/QuickFacts';
 import RulerQuickFieldsContainer from '../components/RulerQuickFieldsContainer';
 import SecondaryHeader from '../components/SecondaryHeader';
+import { useNavigate, useParams } from 'react-router-dom';
+import updateWindowTitle from '../utils/updateWindowTitle';
 
 function RulerPage() {
+	const navigate = useNavigate();
+	const { rulerSlug: slug } = useParams();
+
+	const [loading, setLoading] = useState(false);
+	const [ruler, setRuler] = useState({});
+	const [title, setTitle] = useState(
+		'Itihaas | The Front Page of Indian History'
+	);
+
+	const [error, setError] = useState({
+		state: false,
+		prompt: '',
+	});
+
+	useEffect(
+		function () {
+			async function fetchRuler() {
+				try {
+					setLoading(true);
+					setError(function (current) {
+						return {
+							...current,
+							state: false,
+							prompt: '',
+						};
+					});
+
+					const BASE_URL = import.meta.env.VITE_BASE_SERVER_URI;
+					const response = await fetch(`${BASE_URL}/rulers/${slug}`);
+					const data = await response.json();
+					console.log(data);
+
+					if (response?.ok && data?.success) {
+						setRuler(data?.data?.ruler);
+						updateWindowTitle(setTitle, data?.data?.ruler?.name);
+					} else if (!response.ok && response.status === 404) {
+						navigate('/not-found');
+					} else {
+						throw new Error();
+					}
+				} catch {
+					setRuler({});
+					setError(function (current) {
+						return {
+							...current,
+							state: false,
+							prompt: '',
+						};
+					});
+				} finally {
+					setLoading(false);
+				}
+			}
+
+			fetchRuler();
+		},
+		[slug, navigate]
+	);
+
 	return (
 		<>
 			<Navbar />
