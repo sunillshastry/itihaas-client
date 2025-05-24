8d0ba8c0340a13b322ee23f4bcf53435aab1a8d8
sunil <sunil.shastry69@gmail.com>
2025-05-23 20:08:31 -0600

    Remove functionality to hide dropdown on click elsewhere

--- src/components/CiteDropdown.jsx
+++ src/components/CiteDropdown.jsx
@@ -1,31 +1,13 @@
 import { ClipboardCopy, MessageSquareQuote } from 'lucide-react';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
+import CiteListDropdown from './CiteListDropdown';
 
 function CiteDropdown() {
 	const [isCiteTabOpen, setIsCiteTabOpen] = useState(false);
-	const citeTabRef = useRef(null);
-	const citeBtnRef = useRef(null);
-
-	useEffect(function () {
-		function removeTabVisibility(e) {
-			if (
-				citeTabRef.current &&
-				!citeTabRef.current.contains(e.target) &&
-				citeBtnRef.current &&
-				!citeBtnRef.current.contains(e.target)
-			) {
-				setIsCiteTabOpen(false);
-			}
-		}
-
-		document.addEventListener('click', removeTabVisibility);
-		return () => document.removeEventListener('click', removeTabVisibility);
-	}, []);
 
 	return (
 		<div className="relative">
 			<button
-				ref={citeBtnRef}
 				className="border-primary text-primary from-primary-80 to-primary-90 hover:text-primary-200 hover:border-primary-200 flex items-center rounded-sm border bg-linear-to-r px-5 py-2 font-medium hover:cursor-pointer"
 				onClick={() => setIsCiteTabOpen((current) => !current)}
 			>
@@ -36,15 +18,9 @@ function CiteDropdown() {
 			</button>
 
 			{isCiteTabOpen && (
-				<div
-					ref={citeTabRef}
-					className="bg-primary-90 after:border-r-primary-90 absolute -left-full mt-3 max-w-md -translate-x-0.5 rounded-xs p-3 shadow-md shadow-black/35 after:absolute after:top-[-10px] after:right-0 after:z-10 after:h-0 after:w-0 after:border-[10px] after:border-transparent after:content-['']"
-				>
-					<select>
-						<option value="harvard">Harvard</option>
-						<option value="chicago">Chicago</option>
-						<option value="apa">APA</option>
-					</select>
+				<div className="bg-primary-90 after:border-r-primary-90 absolute -left-full mt-3 max-w-md -translate-x-0.5 rounded-xs p-3 shadow-md shadow-black/35 after:absolute after:top-[-10px] after:right-0 after:z-10 after:h-0 after:w-0 after:border-[10px] after:border-transparent after:content-['']">
+					<CiteListDropdown />
+
 					<p className="text-primary-200 mt-2 text-sm font-medium italic">
 						2025. Accessed May 24.
 						https://chatgpt.com/c/6831127f-a370-8000-9fbd-f66069494e2e.
