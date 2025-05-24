aec703a1140737bdbce0c326f3b7b9c32e9bcb5d
sunil <sunil.shastry69@gmail.com>
2025-05-24 00:59:42 -0600

    Document utils/formatArrayToString.jsx source file

--- src/utils/formatArrayToString.jsx
+++ src/utils/formatArrayToString.jsx
@@ -1,3 +1,9 @@
+/**
+ * Converts all items in an array into a single, formatted string value
+ *
+ * @param {array} array A list of arrays
+ * @returns Combined values of the array as a single string
+ */
 function formatArrayToString(array) {
 	return array.join(', ');
 }
