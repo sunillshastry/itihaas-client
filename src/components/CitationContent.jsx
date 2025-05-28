f7ba1d7a3b03081bf265266b3fdf213aeb2a3dbd
sunil <sunil.shastry69@gmail.com>
2025-05-28 17:11:04 -0600

    Include CopyToClipboard component witin CitationContent with logic

--- src/components/CitationContent.jsx
+++ src/components/CitationContent.jsx
@@ -1,8 +1,12 @@
 import { ClipboardCopy } from 'lucide-react';
 import CiteListDropdown from './CiteListDropdown';
 import PropTypes from 'prop-types';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import { useState } from 'react';
 
 function CitationContent({ citation, onOptionChange }) {
+	const [copied, setCopied] = useState(false);
+
 	return (
 		<div className="bg-primary-90 after:border-r-primary-90 citation-tab absolute mt-3 min-w-2xs rounded-xs p-3 shadow-md shadow-black/35 after:absolute after:top-[-10px] after:right-0 after:z-10 after:h-0 after:w-0 after:border-[10px] after:border-transparent after:content-['']">
 			<CiteListDropdown onChange={onOptionChange} />
@@ -11,15 +15,25 @@ function CitationContent({ citation, onOptionChange }) {
 				{citation}
 			</p>
 			<div className="flex items-center">
-				<button
-					className="text-primary border-primary hover:text-primary-300 mt-2 rounded-sm border bg-white p-1 hover:cursor-pointer"
-					title="Copy Citation"
+				<CopyToClipboard
+					text={citation}
+					onCopy={() => {
+						setCopied(true);
+						setTimeout(() => setCopied(false), 800);
+					}}
 				>
-					<ClipboardCopy size={18} />
-				</button>
-				<label className="text-primary mt-2 ml-1 hidden text-xs font-bold uppercase">
-					Copied!
-				</label>
+					<button
+						className="text-primary border-primary hover:text-primary-300 mt-2 rounded-sm border bg-white p-1 hover:cursor-pointer"
+						title="Copy Citation"
+					>
+						<ClipboardCopy size={18} />
+					</button>
+				</CopyToClipboard>
+				{copied && (
+					<label className="text-primary mt-2 ml-1 text-xs font-bold uppercase">
+						Copied!
+					</label>
+				)}
 			</div>
 		</div>
 	);
