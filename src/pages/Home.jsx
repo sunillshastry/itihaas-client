60981c9158283cd47cbfc13ad632386ae1d60324
sunil <sunil.shastry69@gmail.com>
2025-05-28 20:47:36 -0600

    Include the Offers.jsx ui component in the Home.jsx page component file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import About from '../ui/About';
+import Offers from '../ui/Offers';
 
 function Home() {
 	return (
 		<>
 			<main className="home__main px-20">
-				<section className="home__main--primary bg-brown-background px-3">
+				<section className="home__main--primary bg-brown-background px-7">
 					<Navbar />
 					<About />
+					<Offers />
 				</section>
 			</main>
 			<Footer />
