d66f7be5b93c41fd21f9d001480e2db9b60fadb2
sunil <sunil.shastry69@gmail.com>
2025-05-28 21:28:52 -0600

    Include UseAPI component and update <main> styling for Footer component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,18 @@ import Navbar from '../components/Navbar';
 import About from '../ui/About';
 import Offers from '../ui/Offers';
 import TheWhy from '../ui/TheWhy';
+import UseAPI from '../ui/UseAPI';
 
 function Home() {
 	return (
 		<>
-			<main className="home__main px-20">
-				<section className="home__main--primary bg-brown-background px-7">
+			<main className="home__main px-20 pb-36">
+				<section className="home__main--primary bg-brown-background px-7 pb-5">
 					<Navbar />
 					<About />
 					<Offers />
 					<TheWhy />
+					<UseAPI />
 				</section>
 			</main>
 			<Footer />
