95d59ce32ece7aab5898764ae5f4f9f1ead67e8a
sunil <sunil.shastry69@gmail.com>
2025-06-18 19:14:41 -0600

    Update Vite configuration to include custom alias to 'src' directory as '@'

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,5 +14,11 @@ export default defineConfig({
 	preview: {
 		host: '0.0.0.0',
 		port: 3001,
+	},
+	resolve: {
+		alias: {
+			// eslint-disable-next-line no-undef
+			'@': path.resolve(__dirname, './src'),
+		},
 	},
 });
