d5f9cc73ec13a17766d67e1a564c9dd247f52573
sunil <sunil.shastry69@gmail.com>
2025-06-18 20:26:29 -0600

    Update and refactor to incorporate react-query fetching in DynastiesPage, instead of useEffect manual fetching

--- src/pages/DynastiesPage.jsx
+++ src/pages/DynastiesPage.jsx
@@ -9,6 +9,8 @@ import DynastyPageList from '@/components/dynasty/DynastyPageList';
 import Footer from '@/components/elements/Footer';
 import FetchFailComponent from '@/components/elements/FetchFailComponent';
 import EntitiesPageNoResult from '@/components/views/EntitiesPageNoResult';
+import { useQuery } from '@tanstack/react-query';
+import getDynasties from '@/api/getDynasties';
 
 /**
  * Main React.JSX page component for /dynasties: Dynasties page
@@ -17,65 +19,18 @@ import EntitiesPageNoResult from '@/components/views/EntitiesPageNoResult';
  */
 function DynastiesPage() {
 	// State
-	const [dynasties, setDynasties] = useState([]);
 	const [queriedDynasties, setQueriedDynasties] = useState([]);
-	const [loading, setLoading] = useState(false);
 	const [searchQuery, setSearchQuery] = useState('');
 
-	const [error, setError] = useState({
-		state: false,
-		prompt: '',
+	// Data fetching (React Query)
+	const {
+		data: dynasties,
+		isPending,
+		error,
+	} = useQuery({
+		queryKey: ['dynasties'],
+		queryFn: getDynasties,
 	});
-
-	// Effects
-	useEffect(function () {
-		/**
-		 * Fetches all dynasties from the backend via async/await fetch request(s)
-		 */
-		async function fetchAllDynasties() {
-			try {
-				// Update error and loading to null, false respectively
-				setError(function (current) {
-					return {
-						...current,
-						state: false,
-						prompt: '',
-					};
-				});
-
-				setLoading(true);
-
-				// Make a fetch request
-				const BASE_URL = import.meta.env.VITE_BASE_SERVER_URI;
-				const response = await fetch(`${BASE_URL}/dynasties`);
-				const data = await response.json();
-
-				// Check for success/failure responses
-				if (response.ok && data?.success) {
-					setDynasties(data?.data?.dynasties);
-				} else {
-					throw new Error();
-				}
-			} catch {
-				// Handle failed fetch response
-				setQueriedDynasties([]);
-				setDynasties([]);
-
-				setError(function (current) {
-					return {
-						...current,
-						state: true,
-						prompt: 'Failed to fetch data from the backend. Please try again',
-					};
-				});
-			} finally {
-				// Update loading status
-				setLoading(false);
-			}
-		}
-
-		fetchAllDynasties();
-	}, []);
 
 	useEffect(function () {
 		// Update document title field
@@ -110,7 +65,7 @@ function DynastiesPage() {
 			<MainContainer>
 				<div>
 					<PrimaryHeader>Dynasties</PrimaryHeader>
-					{!error.state && (
+					{!error && (
 						<PageSearchBar
 							className="mt-5"
 							placeholder="Search all dynasties..."
@@ -121,9 +76,9 @@ function DynastiesPage() {
 					)}
 				</div>
 
-				{error.state && <FetchFailComponent />}
+				{error && <FetchFailComponent />}
 
-				{loading ? (
+				{isPending ? (
 					<Loader />
 				) : (
 					<>
