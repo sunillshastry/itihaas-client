a95cf7ba86056c3135c597df024692ddae7f6b5c
sunil <sunil.shastry69@gmail.com>
2025-06-18 20:09:27 -0600

    Update the primary render ReactQueryDevtools tag with the buttonPosition attribute; via the newly created data/config file for button position

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import '@/index.css';
 import App from '@/App.jsx';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import REACT_QUERY_DEV_TOOLS_POSITION from '@/data/reactQueryDevToolsPosition';
 
 const queryClient = new QueryClient();
 
@@ -12,7 +13,10 @@ if (import.meta.env.VITE_APP_ENV === 'development') {
 	createRoot(document.getElementById('root')).render(
 		<StrictMode>
 			<QueryClientProvider client={queryClient}>
-				<ReactQueryDevtools initialIsOpen={false} />
+				<ReactQueryDevtools
+					initialIsOpen={false}
+					buttonPosition={REACT_QUERY_DEV_TOOLS_POSITION}
+				/>
 				<Router>
 					<App />
 				</Router>
