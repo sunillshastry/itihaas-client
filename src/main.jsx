9a72a116714802495cf9cf1f1629530056278399
sunil <sunil.shastry69@gmail.com>
2025-06-18 20:07:10 -0600

    Setup @tanstack/react-query configuration in main.jsx source file

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,28 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '@/index.css';
 import App from '@/App.jsx';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 if (import.meta.env.VITE_APP_ENV === 'development') {
 	createRoot(document.getElementById('root')).render(
 		<StrictMode>
-			<Router>
-				<App />
-			</Router>
+			<QueryClientProvider client={queryClient}>
+				<ReactQueryDevtools initialIsOpen={false} />
+				<Router>
+					<App />
+				</Router>
+			</QueryClientProvider>
 		</StrictMode>
 	);
 } else {
 	createRoot(document.getElementById('root')).render(
-		<Router>
-			<App />
-		</Router>
+		<QueryClientProvider client={queryClient}>
+			<Router>
+				<App />
+			</Router>
+		</QueryClientProvider>
 	);
 }
