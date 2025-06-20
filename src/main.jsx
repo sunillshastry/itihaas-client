ddc8d3b8c1f3885a91e3cb331366fb61e4aede53
sunil <sunil.shastry69@gmail.com>
2025-06-20 16:04:25 -0600

    Wrap CitationContextProvider tag within main app

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import REACT_QUERY_DEV_TOOLS_POSITION from '@/data/reactQueryDevToolsPosition';
 import PlatformGrowthProvider from '@/context/PlatformGrowthContext';
+import CitationContextProvider from './context/CitationContext';
 
 const queryClient = new QueryClient();
 
@@ -15,13 +16,15 @@ if (import.meta.env.VITE_APP_ENV === 'development') {
 		<StrictMode>
 			<QueryClientProvider client={queryClient}>
 				<PlatformGrowthProvider>
-					<ReactQueryDevtools
-						initialIsOpen={false}
-						buttonPosition={REACT_QUERY_DEV_TOOLS_POSITION}
-					/>
-					<Router>
-						<App />
-					</Router>
+					<CitationContextProvider>
+						<ReactQueryDevtools
+							initialIsOpen={false}
+							buttonPosition={REACT_QUERY_DEV_TOOLS_POSITION}
+						/>
+						<Router>
+							<App />
+						</Router>
+					</CitationContextProvider>
 				</PlatformGrowthProvider>
 			</QueryClientProvider>
 		</StrictMode>
@@ -30,9 +33,11 @@ if (import.meta.env.VITE_APP_ENV === 'development') {
 	createRoot(document.getElementById('root')).render(
 		<QueryClientProvider client={queryClient}>
 			<PlatformGrowthProvider>
-				<Router>
-					<App />
-				</Router>
+				<CitationContextProvider>
+					<Router>
+						<App />
+					</Router>
+				</CitationContextProvider>
 			</PlatformGrowthProvider>
 		</QueryClientProvider>
 	);
