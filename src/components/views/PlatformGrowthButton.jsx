a5d7b83469c56afdedc12a806d657a2e06545e21
sunil <sunil.shastry69@gmail.com>
2025-06-20 13:30:10 -0600

    Update styling for PlatformGrowthButton: change font-weight

--- src/components/views/PlatformGrowthButton.jsx
+++ src/components/views/PlatformGrowthButton.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 function PlatformGrowthButton({ click }) {
 	return (
 		<button
-			className="bg-primary-300 text-sm flex items-center py-1 px-2 rounded-xl mt-2 hover:cursor-pointer hover:bg-primary-200 text-primary-50 mb-5"
+			className="bg-primary-300 text-sm flex items-center py-1 px-2 rounded-xl mt-2 hover:cursor-pointer hover:bg-primary-200 text-primary-50 mb-5 font-medium"
 			onClick={click}
 		>
 			<span>
