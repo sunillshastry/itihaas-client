9af2c6f940f411e778febff96c79fe1a89685924
sunil <sunil.shastry69@gmail.com>
2025-06-20 13:59:18 -0600

    Include PlatformGrowthContext Provider in main.jsx source entry file: both for dev and production mode(s)

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from '@/App.jsx';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import REACT_QUERY_DEV_TOOLS_POSITION from '@/data/reactQueryDevToolsPosition';
+import PlatformGrowthProvider from './context/PlatformGrowthContext';
 
 const queryClient = new QueryClient();
 
@@ -13,22 +14,26 @@ if (import.meta.env.VITE_APP_ENV === 'development') {
 	createRoot(document.getElementById('root')).render(
 		<StrictMode>
 			<QueryClientProvider client={queryClient}>
-				<ReactQueryDevtools
-					initialIsOpen={false}
-					buttonPosition={REACT_QUERY_DEV_TOOLS_POSITION}
-				/>
-				<Router>
-					<App />
-				</Router>
+				<PlatformGrowthProvider>
+					<ReactQueryDevtools
+						initialIsOpen={false}
+						buttonPosition={REACT_QUERY_DEV_TOOLS_POSITION}
+					/>
+					<Router>
+						<App />
+					</Router>
+				</PlatformGrowthProvider>
 			</QueryClientProvider>
 		</StrictMode>
 	);
 } else {
 	createRoot(document.getElementById('root')).render(
 		<QueryClientProvider client={queryClient}>
-			<Router>
-				<App />
-			</Router>
+			<PlatformGrowthProvider>
+				<Router>
+					<App />
+				</Router>
+			</PlatformGrowthProvider>
 		</QueryClientProvider>
 	);
 }
