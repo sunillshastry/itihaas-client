d3e9a300c3c78b573b17362ef8ea27349b0a6e2c
sunil <sunil.shastry69@gmail.com>
2025-07-08 12:52:58 -0600

    Refactor and remove typescript errors and warnings

--- src/utils/updateWindowTitle.tsx
+++ src/utils/updateWindowTitle.tsx
@@ -4,7 +4,10 @@
  * @param {function} setTitle A useState function to set the title state
  * @param {string} name Name of the page/entity that must be set in the title field
  */
-function updateWindowTitle(setTitle, name) {
+function updateWindowTitle(
+	setTitle: React.Dispatch<React.SetStateAction<string>>,
+	name: string
+): void {
 	setTitle(`Itihaas | ${name} | The Front Page of Indian History`);
 }
 
