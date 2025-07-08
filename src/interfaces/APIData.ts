354d21f9c0ac9793f639b3177cb60e1d26948437
sunil <sunil.shastry69@gmail.com>
2025-07-08 14:42:27 -0600

    Update APIData to have better type inference for dynasties and dynasty

--- src/interfaces/APIData.ts
+++ src/interfaces/APIData.ts
@@ -1,14 +1,19 @@
 import { Dynasty } from './Dynasty';
 import { StandaloneDynasty } from './StandaloneDynasty';
 
-type ValidKeys = 'dynasties' | 'dynasty' | 'rulers' | 'ruler';
+interface DataValue {
+	dynasties?: Dynasty[];
+
+	dynasty?: StandaloneDynasty;
+
+	// rulers?: null;
+	// ruler?: null;
+}
 
 interface Data {
 	success: boolean;
 	size?: number;
-	data: {
-		[Key in ValidKeys]?: StandaloneDynasty | Dynasty[];
-	};
+	data: DataValue;
 }
 
 export type { Data };
