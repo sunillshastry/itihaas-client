c70bdeb38b293b93d32a6db65a03f7919020fd82
sunil <sunil.shastry69@gmail.com>
2025-07-08 12:50:18 -0600

    Refactor and remove typescript errors and warnings

--- src/utils/convertToTwelveHours.ts
+++ src/utils/convertToTwelveHours.ts
@@ -1,12 +1,15 @@
+type FuncReturnType = [number, string];
+
 /**
  * Convert 24 hour value to its 12 hour equivalent
  *
  * @param {number} hours The 24-hour value
  * @returns The twelve hour equivalent of a 24-hour value
  */
-function convertToTwelveHours(hours) {
-	const time = ['', ''];
+function convertToTwelveHours(hours: number): FuncReturnType {
+	const time: FuncReturnType = [0, ''];
 	const meridian = hours >= 12 ? 'PM' : 'AM';
+
 	time[1] = meridian;
 	time[0] = hours % 12;
 	time[0] = time[0] === 0 ? 12 : time[0];
