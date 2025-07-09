8a451e8db2c7c54ac88aab62a1fa71b1a328be23
sunil <sunil.shastry69@gmail.com>
2025-07-09 04:28:28 -0600

    Move index.css stylesheet to dedicated styles folder and update main.tsx entry file for import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '@/index.css';
+import '@/styles/index.css';
 import App from '@/App.jsx';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
