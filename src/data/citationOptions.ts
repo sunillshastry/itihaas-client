684d610c3c610dab56ccee6a48a06c7390b01095
sunil <sunil.shastry69@gmail.com>
2025-07-09 14:38:03 -0600

    Incorporate newly created CitationFormat interface within citationOptions data source

--- src/data/citationOptions.ts
+++ src/data/citationOptions.ts
@@ -1,6 +1,10 @@
-export default [
+import { CitationFormat } from '@/interfaces/CitationFormat';
+
+const options: CitationFormat[] = [
 	{ value: 'apa', label: 'APA' },
 	{ value: 'chicago', label: 'Chicago' },
 	{ value: 'mla', label: 'MLA' },
 	{ value: 'harvard', label: 'Harvard' },
 ];
+
+export default options;
