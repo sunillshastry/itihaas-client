e9519aced15bd1217325c267be969dccc54c05bb
sunil <sunil.shastry69@gmail.com>
2025-07-09 14:39:24 -0600

    Specify custom position type for @tanstack-query/dev-tools position value in data source

--- src/data/reactQueryDevToolsPosition.ts
+++ src/data/reactQueryDevToolsPosition.ts
@@ -1,6 +1,9 @@
+import { ReactQueryDevToolsPosition } from '@/types/ReactQueryDevPosition';
+
 /**
  * Possible options for positions: "top-left" | "top-right" | "bottom-left" | "bottom-right" | "relative"
  */
-const REACT_QUERY_DEV_TOOLS_POSITION = 'bottom-left';
+const REACT_QUERY_DEV_TOOLS_POSITION: ReactQueryDevToolsPosition =
+	'bottom-left';
 
 export default REACT_QUERY_DEV_TOOLS_POSITION;
