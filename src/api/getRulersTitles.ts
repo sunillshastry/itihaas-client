1b55e13423d4ce230fbf1aa4c10e1003268321e3
sunil <sunil.shastry69@gmail.com>
2025-07-09 00:13:58 -0600

    Refactor getRulerTitles to have interfaces/type support

--- src/api/getRulersTitles.ts
+++ src/api/getRulersTitles.ts
@@ -1,14 +1,16 @@
+import { SearchData } from '@/interfaces/APISearchData';
+
 /**
  * Get all ruler titles from the server API
  *
  * @returns A Promise consisting of the API response
  */
-async function getRulersTitles() {
+async function getRulersTitles(): Promise<string[] | Error | undefined> {
 	const BASE_URL = import.meta.env.VITE_BASE_SERVER_URI;
 
 	try {
 		// Fetch initial response
-		const response = await fetch(
+		const response: Response = await fetch(
 			`${BASE_URL}/rulers/search/titles?include=id,slug,type`
 		);
 
@@ -17,12 +19,12 @@ async function getRulersTitles() {
 			throw new Error('Error: Failed to fetch ruler search titles');
 		}
 
-		const data = await response.json();
+		const data: SearchData = await response.json();
 
 		return data?.data?.rulers || [];
 	} catch (e) {
 		// Catch block: Return the error object itself
-		return e;
+		if (e instanceof Error) return e;
 	}
 }
 
