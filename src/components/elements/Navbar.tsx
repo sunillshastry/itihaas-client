1cb4f540c25d84eab9d9f1761c91b39fba4de852
sunil <sunil.shastry69@gmail.com>
2025-07-10 17:49:19 -0600

    Implement handleSearchFormSubmit event handler, with logic and perform programmatic navigation with query term(s)

--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -1,10 +1,22 @@
 import NavbarHeader from '@/components/views/NavbarHeader';
 import NavbarLinksContainer from '@/components/elements/NavbarLinksContainer';
+import { FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
+	const navigate = useNavigate();
+
+	function handleSearchFormSubmit(e: FormEvent, value: string): void {
+		e.preventDefault();
+
+		if (value.length > 3) {
+			navigate(`/search?q=${value}`);
+		}
+	}
+
 	return (
 		<nav className="flex flex-col items-center justify-center pt-10 pb-5">
-			<NavbarHeader />
+			<NavbarHeader onSubmit={handleSearchFormSubmit} />
 			<NavbarLinksContainer />
 		</nav>
 	);
