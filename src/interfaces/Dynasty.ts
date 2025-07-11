4e40c6cb6491c554120df32f5389c0da1528e22a
sunil <sunil.shastry69@gmail.com>
2025-07-10 20:09:03 -0600

    Append Standalone interface fields to default Dynasty and Ruler fields

--- src/interfaces/Dynasty.ts
+++ src/interfaces/Dynasty.ts
@@ -1,12 +1,19 @@
+import { Article } from './Article';
+import { Description } from './Description';
+import { FurtherReading } from './FurtherReading';
+
 interface Dynasty {
 	timeline: {
 		begin: string;
 		end: string;
 	} | null;
 
-	description: {
-		oneline: string | null;
-	};
+	area?: {
+		lowest: string;
+		highest: string;
+	} | null;
+
+	description: Description | null;
 
 	_id: string;
 	slug: string;
@@ -16,8 +23,20 @@ interface Dynasty {
 	locations: string[];
 	population: string | null;
 
+	languages?: string[];
+	religions?: string[];
+	rulers?: string[];
+	currencies?: string[];
+	wars?: string[];
+	sources?: string[];
+	articles?: Article[];
+	furtherReading?: FurtherReading[];
+
 	entity?: string;
 	__v?: string;
+
+	createdAt?: Date;
+	updatedAt?: Date;
 }
 
 export type { Dynasty };
--- src/interfaces/Ruler.ts
+++ src/interfaces/Ruler.ts
@@ -1,12 +1,12 @@
+import { Article } from './Article';
+import { Description } from './Description';
+import { FurtherReading } from './FurtherReading';
+
 interface Ruler {
 	timeline: {
 		begin: string;
 		end: string;
 	} | null;
-
-	description: {
-		oneline: string;
-	};
 
 	_id: string;
 	slug: string;
@@ -17,6 +17,27 @@ interface Ruler {
 	dynasty: string;
 
 	entity?: string;
+	family?: {
+		father: string;
+		mother: string;
+		children: string[];
+		wives: string[];
+	} | null;
+	description: Description | null;
+
+	religion?: string;
+	predecessor?: string;
+	successor?: string;
+
+	wars?: string[];
+	sources?: string[];
+
+	furtherReading?: FurtherReading[];
+
+	articles?: Article[];
+
+	createdAt?: Date;
+	updatedAt?: Date;
 
 	__v?: string;
 }
