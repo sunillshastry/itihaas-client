0a8af87a777fa564322bdfe73f775cbb1525f7fa
sunil <sunil.shastry69@gmail.com>
2025-07-10 18:09:27 -0600

    Create and implement getQueryResults to combine dynasty and ruler results into one singleton function

---
+++ src/api/getQueryResults.ts
@@ -0,0 +1,23 @@
+import { Dynasty } from '@/interfaces/Dynasty';
+import getDynastyByQuery from './getDynastyByQuery';
+import getRulerByQuery from './getRulerByQuery';
+import { Ruler } from '@/interfaces/Ruler';
+
+async function getQueryResults(query: string) {
+	try {
+		// Dynasty fetch responses
+		const dynastyResponse = (await getDynastyByQuery(query)) as Dynasty[];
+
+		// Ruler fetch response
+		const rulerResponse = (await getRulerByQuery(query)) as Ruler[];
+
+		// Combine both response into one single array and return the result
+		const searchResults = [...dynastyResponse, ...rulerResponse];
+
+		return searchResults;
+	} catch (e) {
+		if (e instanceof Error) return e;
+	}
+}
+
+export default getQueryResults;
