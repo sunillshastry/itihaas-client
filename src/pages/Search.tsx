6955839d35343c6be3ec21aa5623c7d5f9d2db75
sunil <sunil.shastry69@gmail.com>
2025-07-10 20:46:14 -0600

    Author the Search page component with other components, successfully display error state, loading state, empty state and success state. Currently functioning as expected

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,14 +1,92 @@
+import getQueryResults from '@/api/getQueryResults';
+import AddingNewContentUI from '@/components/elements/AddingNewContentUI';
+import FetchFailComponent from '@/components/elements/FetchFailComponent';
+import Footer from '@/components/elements/Footer';
+import Loader from '@/components/elements/Loader';
+import MainContainer from '@/components/elements/MainContainer';
 import Navbar from '@/components/elements/Navbar';
+import PrimaryHeader from '@/components/elements/PrimaryHeader';
+import SearchResult from '@/components/elements/SearchResult';
+import EmptySearchResult from '@/components/views/EmptySearchResult';
+import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
 
 function Search() {
-	return (
-		<>
-			<Navbar />
-			<main>
-				<h1>Search</h1>
-			</main>
-		</>
-	);
+	const [params] = useSearchParams();
+
+	const searchQuery = params.get('q')?.trim();
+
+	const {
+		data: searchResults,
+		isPending,
+		error,
+		isLoading,
+	} = useQuery({
+		queryKey: ['full-search', searchQuery],
+		queryFn: () => getQueryResults(searchQuery as string),
+	});
+
+	// Error State (API not working or other internal error)
+	if (error || searchResults instanceof Error) {
+		return (
+			<>
+				<Navbar />
+				<MainContainer>
+					<FetchFailComponent />
+				</MainContainer>
+				<Footer className="mt-36" />
+			</>
+		);
+	}
+
+	// Loading state
+	if (isPending || isLoading) {
+		return (
+			<>
+				<Navbar />
+				<MainContainer>
+					<Loader />
+				</MainContainer>
+				<Footer className="mt-36" />
+			</>
+		);
+	}
+
+	if (searchResults !== undefined && !(searchResults instanceof Error)) {
+		return (
+			<>
+				<Navbar />
+				<MainContainer>
+					<div>
+						<AddingNewContentUI />
+						<PrimaryHeader>
+							Found{' '}
+							{searchResults.length < 10 && searchResults.length !== 0
+								? `0${searchResults.length}`
+								: searchResults.length}{' '}
+							results for '{searchQuery}'
+						</PrimaryHeader>
+					</div>
+
+					{searchResults.length > 0 ? (
+						<ul className="mt-10">
+							{searchResults.map(function (searchResult) {
+								return (
+									<SearchResult
+										key={searchResult._id}
+										entity={searchResult}
+									/>
+								);
+							})}
+						</ul>
+					) : (
+						<EmptySearchResult />
+					)}
+				</MainContainer>
+				<Footer className="mt-36" />
+			</>
+		);
+	}
 }
 
 export default Search;
