aa856b7af41c1fc836f281872d40caa870050ac6
sunil <sunil.shastry69@gmail.com>
2025-07-10 22:49:52 -0600

    Remove manually created 'div' and replace it with SearchResultInfoPiece component

--- src/components/views/SearchResultInfo.tsx
+++ src/components/views/SearchResultInfo.tsx
@@ -1,4 +1,5 @@
 import { BookOpenText, Newspaper, ScrollText } from 'lucide-react';
+import SearchResultInfoPiece from './SearchResultInfoPiece';
 
 interface FunctionProps {
 	sources: number;
@@ -9,33 +10,27 @@ interface FunctionProps {
 function SearchResultInfo({ sources, articles, readings }: FunctionProps) {
 	return (
 		<div className="text-primary-300 mt-3 flex items-center justify-start gap-3 text-sm">
-			<div className="flex items-center justify-start gap-1">
-				<span>
-					<ScrollText size={18} />
-				</span>
-				<span className="font-medium">
-					{sources < 10 && sources !== 0 ? `0${sources}` : sources} Sources
-				</span>
-			</div>
+			<SearchResultInfoPiece
+				field={sources}
+				fieldName="Source"
+				className="ml-0"
+			>
+				<ScrollText size={18} />
+			</SearchResultInfoPiece>
 
-			<div className="ml-2 flex items-center justify-start gap-1">
-				<span>
-					<Newspaper size={18} />
-				</span>
-				<span className="font-medium">
-					{articles < 10 && articles !== 0 ? `0${articles}` : articles} Articles
-				</span>
-			</div>
+			<SearchResultInfoPiece
+				field={articles}
+				fieldName="Article"
+			>
+				<Newspaper size={18} />
+			</SearchResultInfoPiece>
 
-			<div className="ml-2 flex items-center justify-start gap-1">
-				<span>
-					<BookOpenText size={18} />
-				</span>
-				<span className="font-medium">
-					{readings < 10 && readings !== 0 ? `0${readings}` : readings} Further
-					Readings
-				</span>
-			</div>
+			<SearchResultInfoPiece
+				field={readings}
+				fieldName="Reading"
+			>
+				<BookOpenText size={18} />
+			</SearchResultInfoPiece>
 		</div>
 	);
 }
