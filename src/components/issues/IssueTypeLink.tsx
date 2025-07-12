dfd42ca8f433b960742c49e5b48a83a153ec93e4
sunil <sunil.shastry69@gmail.com>
2025-07-11 22:34:39 -0600

    Add a group hover state for IssueTypeLink component

--- src/components/issues/IssueTypeLink.tsx
+++ src/components/issues/IssueTypeLink.tsx
@@ -12,7 +12,7 @@ function IssueTypeLink({ title, linkContent, children, to }: FunctionProps) {
 	return (
 		<Link
 			to={to}
-			className="border-primary-50 bg-primary-50 hover:bg-primary-60 my-6 block rounded-sm border p-3 shadow transition ease-in hover:cursor-pointer hover:shadow-lg"
+			className="border-primary-50 bg-primary-50 hover:bg-primary-60 group my-6 block rounded-sm border p-3 shadow transition ease-in hover:cursor-pointer hover:shadow-lg"
 		>
 			<h3 className="border-primary/35 border-b text-lg font-medium">
 				{title}
@@ -20,9 +20,9 @@ function IssueTypeLink({ title, linkContent, children, to }: FunctionProps) {
 
 			<p className="text-primary-100 mt-2 italic">{children}</p>
 
-			<button className="bg-primary-700 text-primary-60 mt-4 flex cursor-pointer items-center gap-2 rounded-full px-2.5 py-0.5 text-xs">
+			<button className="bg-primary-700 text-primary-60 mt-4 flex cursor-pointer items-center gap-1 rounded-full px-2.5 py-0.5 text-xs transition">
 				<span>{linkContent}</span>
-				<span>
+				<span className="transition group-hover:translate-x-[3px]">
 					<ArrowRight size={14} />
 				</span>
 			</button>
