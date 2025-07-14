171d65680feb738bd53ff45fcfaa98371c22a058
sunil <sunil.shastry69@gmail.com>
2025-07-14 14:43:16 -0600

    Import 'mdx' and use within defineConfig in vite.config.js configuration file

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import path from 'path';
+import mdx from '@mdx-js/rollup';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss(), ViteMinifyPlugin()],
+	plugins: [react(), tailwindcss(), ViteMinifyPlugin(), mdx()],
 	server: {
 		host: '0.0.0.0',
 		port: 3000,
