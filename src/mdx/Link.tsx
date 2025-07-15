020e600def544affcc2b7b981ed25ff21e9cf396
sunil <sunil.shastry69@gmail.com>
2025-07-14 19:42:35 -0600

    Add new prop for Link MDX component: title, and use in component with interface registration

--- src/mdx/Link.tsx
+++ src/mdx/Link.tsx
@@ -1,13 +1,15 @@
 interface FunctionProps {
 	children: Readonly<React.ReactNode>;
 	href: string;
+	title?: string;
 }
 
-function Link({ children, href }: FunctionProps) {
+function Link({ children, href, title }: FunctionProps) {
 	return (
 		<a
 			href={href}
 			className="text-primary-400 hover:text-primary-10 underline"
+			title={title}
 		>
 			{children}
 		</a>
