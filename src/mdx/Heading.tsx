09e9b309227d85debfdab7e4d577350e863986a7
sunil <sunil.shastry69@gmail.com>
2025-07-14 21:22:42 -0600

    Update styling for Heading MDX component

--- src/mdx/Heading.tsx
+++ src/mdx/Heading.tsx
@@ -3,7 +3,11 @@ interface FunctionProps {
 }
 
 function Heading({ children }: FunctionProps) {
-	return <h5 className="text-primary-300 text-base font-medium">{children}</h5>;
+	return (
+		<h5 className="text-primary-300 text-base font-medium underline underline-offset-2">
+			{children}
+		</h5>
+	);
 }
 
 export default Heading;
