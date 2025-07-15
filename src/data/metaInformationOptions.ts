3e1397c08d0a4e676e138f53abcf3a9efab77130
sunil <sunil.shastry69@gmail.com>
2025-07-15 01:09:59 -0600

    Define sidebar options for meta-information subfield

---
+++ src/data/metaInformationOptions.ts
@@ -0,0 +1,15 @@
+import { SidebarSubfield } from '@/interfaces/SidebarSubfield';
+
+const metaInformationOptions: SidebarSubfield[] = [
+	{
+		content: 'Health Check',
+	},
+	{
+		content: 'API Version',
+	},
+	{
+		content: 'API/App Info',
+	},
+];
+
+export default metaInformationOptions;
