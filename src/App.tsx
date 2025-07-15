65a868707b75b9f358e9b5036fc983ec63bd86cd
sunil <sunil.shastry69@gmail.com>
2025-07-15 15:52:02 -0600

    Remove existing setup for routing and import via config

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from '@/pages/Home';
-import DynastiesPage from '@/pages/DynastiesPage';
-import RulersPage from '@/pages/RulersPage';
-import WarsPage from '@/pages/WarsPage';
-import Issues from '@/pages/Issues';
-import Search from '@/pages/Search';
-import Docs from '@/pages/Docs';
-import NotFound from '@/pages/NotFound';
-import DynastyPage from '@/pages/DynastyPage';
-import WarPage from '@/pages/WarPage';
-import RulerPage from '@/pages/RulerPage';
 import BackToTopButton from '@/components/elements/BackToTopButton';
-import NewContribution from './pages/contributions/NewContribution';
-import FixContribution from './pages/contributions/FixContribution';
-import APIContribution from './pages/contributions/APIContribution';
-import FeedbackContribution from './pages/contributions/FeedbackContribution';
-import Register from '@/pages/Register';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import TermsOfService from './pages/TermsOfService';
+import RouterRoutes from './routes/config';
 
 /**
  * Base App component of the application, which is mounted on the 'root' div component
@@ -28,99 +11,15 @@ function App() {
 	return (
 		<>
 			<Routes>
-				<Route
-					path="/"
-					element={<Home />}
-				/>
-
-				<Route
-					path="/dynasties"
-					element={<DynastiesPage />}
-				/>
-
-				<Route
-					path="/dynasties/:dynastySlug"
-					element={<DynastyPage />}
-				/>
-
-				<Route
-					path="/rulers"
-					element={<RulersPage />}
-				/>
-
-				<Route
-					path="/rulers/:rulerSlug"
-					element={<RulerPage />}
-				/>
-
-				<Route
-					path="/wars"
-					element={<WarsPage />}
-				/>
-
-				<Route
-					path="/wars/:warSlug"
-					element={<WarPage />}
-				/>
-
-				<Route
-					path="/issues"
-					element={<Issues />}
-				/>
-
-				<Route
-					path="/issues/new"
-					element={<NewContribution />}
-				/>
-
-				<Route
-					path="/issues/fix"
-					element={<FixContribution />}
-				/>
-
-				<Route
-					path="/issues/api"
-					element={<APIContribution />}
-				/>
-
-				<Route
-					path="/issues/feedback"
-					element={<FeedbackContribution />}
-				/>
-
-				<Route
-					path="/search"
-					element={<Search />}
-				/>
-
-				<Route
-					path="/docs"
-					element={<Docs />}
-				/>
-
-				<Route
-					path="/register"
-					element={<Register />}
-				/>
-
-				<Route
-					path="/privacy"
-					element={<PrivacyPolicy />}
-				/>
-				<Route
-					path="/privacy-policy"
-					element={<PrivacyPolicy />}
-				/>
-				<Route
-					path="/terms-of-service"
-					element={<TermsOfService />}
-				/>
-
-				{/* Wildcard route(s) to handle all other routes as 404 state */}
-				<Route
-					path="*"
-					element={<NotFound />}
-				/>
+				{RouterRoutes.map(function (route) {
+					return (
+						<Route
+							key={route.path}
+							path={route.path}
+							element={route.element}
+						/>
+					);
+				})}
 			</Routes>
 			<BackToTopButton />
 		</>
