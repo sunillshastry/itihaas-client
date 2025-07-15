30796c1920f421e04ae2677d408edd7e3f360137
sunil <sunil.shastry69@gmail.com>
2025-07-14 18:34:34 -0600

    Create and setup configuration for mdxComponents to replace in-built html components with app's custom styled components

---
+++ src/mdx/config.ts
@@ -0,0 +1,21 @@
+import Code from './Code';
+import Content from './Content';
+import Heading from './Heading';
+import Link from './Link';
+import ListItem from './ListItem';
+import MainHeader from './MainHeader';
+import TitleHeader from './TitleHeader';
+
+const mdxComponents = {
+	h1: MainHeader,
+	h2: MainHeader,
+	h3: MainHeader,
+	h4: TitleHeader,
+	h5: Heading,
+	p: Content,
+	li: ListItem,
+	code: Code,
+	a: Link,
+};
+
+export { mdxComponents };
