ddf444aeabc99ba2512928ebc7c76845224a3f44
sunil <sunil.shastry69@gmail.com>
2025-07-14 18:32:42 -0600

    List of optinons with content and optional id? field for getting-started content in documentation

---
+++ src/data/gettingStartedOptions.ts
@@ -0,0 +1,18 @@
+import { SidebarSubfield } from '@/interfaces/SidebarSubfield';
+
+const gettingStartedOptions: SidebarSubfield[] = [
+	{
+		content: 'Viewing and accessing documentation',
+	},
+	{
+		content: 'Request/Response Examples',
+	},
+	{
+		content: 'API registration',
+	},
+	{
+		content: 'Usage and other guidelines',
+	},
+];
+
+export default gettingStartedOptions;
