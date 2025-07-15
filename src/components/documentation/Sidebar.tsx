a292ad105f86d6e85886f49f2e9906912ea02c26
sunil <sunil.shastry69@gmail.com>
2025-07-15 01:09:29 -0600

    Remove hardcoded fields/values and replace them with pre-defined data; Add new Sidebar section: meta information

--- src/components/documentation/Sidebar.tsx
+++ src/components/documentation/Sidebar.tsx
@@ -1,9 +1,13 @@
 import gettingStartedOptions from '@/data/gettingStartedOptions';
 import SidebarItem from './SidebarItem';
+import metaInformationOptions from '@/data/metaInformationOptions';
+import dynastiesSidebarOptions from '@/data/dynastiesSidebarOptions';
+import rulersSidebarOptions from '@/data/rulersSidebarOptions';
+import warsSidebarOptions from '@/data/warsSidebarOptions';
 
 function Sidebar() {
 	return (
-		<aside className="bg-primary-50 sticky top-5 left-0 rounded-md px-6 py-2">
+		<aside className="bg-primary-50 sticky top-2 left-0 h-screen overflow-x-hidden overflow-y-auto rounded-md px-6 py-4 pr-8 pb-5">
 			<h2 className="text-primary-700 border-primary-20 border-b pb-1 text-xl font-medium">
 				API Reference
 			</h2>
@@ -14,41 +18,25 @@ function Sidebar() {
 					subfields={gettingStartedOptions}
 				/>
 
-				<div className="mt-1">
-					<h4 className="text-base font-medium">Dynasties</h4>
-					<ul className="ml-2 text-sm">
-						<li>Get all dynasties</li>
-						<li>Get dynasty by ID parameter</li>
-						<li>Get dynasty by Slug parameter</li>
-						<li>Get list of dynasty titles</li>
-						<li>Get random dynasty</li>
-						<li>Get dynasty by search query</li>
-					</ul>
-				</div>
+				<SidebarItem
+					title="Meta Information"
+					subfields={metaInformationOptions}
+				/>
 
-				<div className="mt-1">
-					<h4 className="text-base font-medium">Rulers</h4>
-					<ul className="ml-2 text-sm">
-						<li>Get all rulers</li>
-						<li>Get ruler by ID parameter</li>
-						<li>Get ruler by Slug parameter</li>
-						<li>Get list of ruler titles</li>
-						<li>Get random ruler</li>
-						<li>Get ruler by search query</li>
-					</ul>
-				</div>
+				<SidebarItem
+					title="Dynasties"
+					subfields={dynastiesSidebarOptions}
+				/>
 
-				<div className="mt-1">
-					<h4 className="text-base font-medium">Wars</h4>
-					<ul className="ml-2 text-sm">
-						<li>Get all wars</li>
-						<li>Get war by ID parameter</li>
-						<li>Get war by Slug parameter</li>
-						<li>Get list of war titles</li>
-						<li>Get random war</li>
-						<li>Get war by search query</li>
-					</ul>
-				</div>
+				<SidebarItem
+					title="Rulers"
+					subfields={rulersSidebarOptions}
+				/>
+
+				<SidebarItem
+					title="Wars"
+					subfields={warsSidebarOptions}
+				/>
 			</div>
 		</aside>
 	);
