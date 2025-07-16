68bdd5fb92d58e93e3f16c12da76b3ae94ee62b0
sunil <sunil.shastry69@gmail.com>
2025-07-15 20:22:57 -0600

    Implement a custom UI Component: Input

---
+++ src/components/elements/Input.tsx
@@ -0,0 +1,21 @@
+import { ComponentPropsWithoutRef } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+function Input({
+	placeholder,
+	className,
+	...defaultProps
+}: ComponentPropsWithoutRef<'input'>) {
+	return (
+		<input
+			placeholder={placeholder}
+			className={twMerge(
+				'bg-primary-90 focus:outline-primary text-primary-500 w-xs rounded-sm px-3 py-1 font-medium focus:outline-3',
+				className
+			)}
+			{...defaultProps}
+		/>
+	);
+}
+
+export default Input;
