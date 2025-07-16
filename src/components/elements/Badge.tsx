8c819b36dfd47721bc42cb6a39d4e6e46635d055
sunil <sunil.shastry69@gmail.com>
2025-07-15 20:22:05 -0600

    Update Badge component to include a 'className' prop

--- src/components/elements/Badge.tsx
+++ src/components/elements/Badge.tsx
@@ -1,12 +1,18 @@
 import { ComponentPropsWithoutRef } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 interface FunctionProps extends ComponentPropsWithoutRef<'span'> {
 	children: Readonly<React.ReactNode>;
 }
 
-function Badge({ children }: FunctionProps) {
+function Badge({ children, className }: FunctionProps) {
 	return (
-		<span className="from-primary-300 to-primary-500 text-primary-90 ml-1 rounded-full bg-linear-to-b px-[8px] py-[1px] font-mono text-xs uppercase">
+		<span
+			className={twMerge(
+				'from-primary-300 to-primary-500 text-primary-90 ml-1 rounded-full bg-linear-to-b px-[8px] py-[1px] font-mono text-xs uppercase',
+				className
+			)}
+		>
 			{children}
 		</span>
 	);
