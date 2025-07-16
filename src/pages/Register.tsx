eadf6b6c9e9361c17c871bf53f3d54b82945a1dc
sunil <sunil.shastry69@gmail.com>
2025-07-15 20:22:36 -0600

    Build the Register page with a structured section and RegisterInfo component in place

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,6 +2,7 @@ import Footer from '@/components/elements/Footer';
 import MainContainer from '@/components/elements/MainContainer';
 import Navbar from '@/components/elements/Navbar';
 import PrimaryHeader from '@/components/elements/PrimaryHeader';
+import RegisterInfo from '@/components/register/RegisterInfo';
 
 function Register() {
 	return (
@@ -9,6 +10,10 @@ function Register() {
 			<Navbar />
 			<MainContainer>
 				<PrimaryHeader>API Registration</PrimaryHeader>
+
+				<section className="mt-5">
+					<RegisterInfo />
+				</section>
 			</MainContainer>
 			<Footer className="mt-36" />
 		</>
