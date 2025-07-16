bfe578a5984543374a7a26372615b06bb65b81d5
sunil <sunil.shastry69@gmail.com>
2025-07-15 20:58:32 -0600

    Add missing 'type' with defaultValue as 'text' and pass prop

--- src/components/elements/Input.tsx
+++ src/components/elements/Input.tsx
@@ -4,10 +4,13 @@ import { twMerge } from 'tailwind-merge';
 function Input({
 	placeholder,
 	className,
+	type = 'text',
 	...defaultProps
 }: ComponentPropsWithoutRef<'input'>) {
 	return (
 		<input
+			type={type}
+			autoComplete="off"
 			placeholder={placeholder}
 			className={twMerge(
 				'bg-primary-90 focus:outline-primary text-primary-500 w-xs rounded-sm px-3 py-1 font-medium focus:outline-3',
