9d529d22039755668ed3c5906c6edec3de5c9202
sunil <sunil.shastry69@gmail.com>
2025-07-17 01:20:43 -0600

    Add new section in RegisterInfo component: API recovery

--- src/components/register/RegisterInfo.tsx
+++ src/components/register/RegisterInfo.tsx
@@ -242,7 +242,7 @@ function RegisterInfo() {
 			<div className="mt-5">
 				<h4 className="text-primary-600 text-lg font-medium">
 					Contributing to Itihaas{' '}
-					<Badge className="p-1 px-1.5">Help wanted!</Badge>
+					<Badge className="rounded-lg p-1 px-1.5">Help wanted!</Badge>
 				</h4>
 
 				<p>
@@ -272,7 +272,45 @@ function RegisterInfo() {
 			</div>
 
 			<div className="mt-5">
-				<SubHeader>Obtain API Key</SubHeader>
+				<h4 className="text-primary-600 text-lg font-medium">API Recovery</h4>
+
+				<p>
+					If you have previously registered for an API key at Itihaas, and have
+					lost the API key, misplaced it, or simply can't find it, do not worry.
+					We have a dedicated section to help you recover it with ease, with
+					minimal verification. Just provide us with the email address
+					associated with your API key, and we will send you an email with the
+					API key, if the email was initially registered with us. Please give it
+					a couple of minutes and check your inbox for an email from Itihaas,
+					check your spam folder if you do not receive the recovery email in
+					under five minutes.
+				</p>
+
+				<p className="mt-1 font-medium">
+					You can recover your API key and view more details about it{' '}
+					<Link
+						to="/api-recovery"
+						className="text-primary-600 hover:text-primary-10 underline"
+					>
+						here
+					</Link>
+					.
+				</p>
+
+				<p className="mt-1 font-medium">
+					For any concerns and issues regarding the API, please check our page{' '}
+					<Link
+						to="/issues/api"
+						className="text-primary-600 hover:text-primary-10 underline"
+					>
+						here
+					</Link>
+					.
+				</p>
+			</div>
+
+			<div className="mt-7">
+				<SubHeader>Obtain New API Key</SubHeader>
 				<p className="mt-3 text-sm">
 					Please use and submit the form below to gain access to your unique
 					Itihaas API key.{' '}
