80b64d128343c56b1115abb17d89418cde3df12b
sunil <sunil.shastry69@gmail.com>
2025-07-19 01:57:23 -0600

    Update rule/option for test command

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,19 @@ export default defineConfig({
 		host: '0.0.0.0',
 		port: 3000,
 	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					react: ['react', 'react-dom'],
+					tanstack: ['@tanstack/react-query', '@tanstack/react-query-devtools'],
+					router: ['react-router-dom'],
+					mdx: ['@mdx-js/react', '@mdx-js/rollup'],
+					form: ['react-hook-form'],
+				},
+			},
+		},
+	},
 	preview: {
 		host: '0.0.0.0',
 		port: 3001,
