145631b419c4eba22845f664021906c4e48387b4
sunil <sunil.shastry69@gmail.com>
2025-07-19 18:24:12 -0600

    Make homepage responsive to all sizes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,8 +8,8 @@ import UseAPI from '@/ui/UseAPI';
 function Home() {
 	return (
 		<>
-			<main className="home__main px-20 pb-36">
-				<section className="home__main--primary bg-brown-background px-7 pb-5">
+			<main className="home__main px-20 pb-36 max-lg:px-10 max-md:px-0 max-md:pb-0">
+				<section className="home__main--primary bg-brown-background px-7 pb-5 max-lg:px-4 max-md:pb-10 max-sm:px-0 max-sm:pb-0">
 					<Navbar />
 					<About />
 					<Offers />
