c142e98337d3fbed377c5885114cdd3b49098fda
sunil <sunil.shastry69@gmail.com>
2025-07-20 15:11:11 -0600

    Add explicit 'width', 'title' and 'height' property to Logo image, also add 'title' property to Logo header

--- src/components/elements/Logo.tsx
+++ src/components/elements/Logo.tsx
@@ -5,8 +5,15 @@ function Logo() {
 				src="/logo.png"
 				alt="Itihaas Logo"
 				width="50"
+				height="50"
+				title="Itihaas"
 			/>
-			<h1 className="font-logo text-primary-500 ml-2 text-3xl">Itihaas</h1>
+			<h1
+				className="font-logo text-primary-500 ml-2 text-3xl"
+				title="Itihaas"
+			>
+				Itihaas
+			</h1>
 		</>
 	);
 }
