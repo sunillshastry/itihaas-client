d1f3a22d4983229bb6178839033a85477b2da4aa
sunil <sunil.shastry69@gmail.com>
2025-08-14 18:38:39 -0600

    Create NewContribution section component with no content

--- src/pages/contributions/NewContribution.tsx
+++ src/pages/contributions/NewContribution.tsx
@@ -3,14 +3,22 @@ import Footer from '@/components/elements/Footer';
 import MainContainer from '@/components/elements/MainContainer';
 import Navbar from '@/components/elements/Navbar';
 import SubHeader from '@/components/elements/SubHeader';
+import AccuracyBlockquote from '@/components/issues/AccuracyBlockquote';
 
 function NewContribution() {
 	return (
 		<>
 			<Navbar />
 			<MainContainer>
-				<BackButton />
-				<SubHeader>New additions</SubHeader>
+				<div>
+					<BackButton />
+					<SubHeader>New additions</SubHeader>
+					<p className="text-primary-600 mt-1 text-sm font-medium">
+						Use this form recommend new entity additions or having any
+						additional sub-fields within existing entities.
+					</p>
+					<AccuracyBlockquote className="mt-4" />
+				</div>
 			</MainContainer>
 			<Footer className="mt-36" />
 		</>
