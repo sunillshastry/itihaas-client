c23fd1a9dc2c277ea850288c3c4030b5d1d5c6e1
sunil <sunil.shastry69@gmail.com>
2025-08-14 21:26:56 -0600

    Add new issue component: TextAreaFormControl for textarea fields

---
+++ src/components/issues/TextAreaFormControl.tsx
@@ -0,0 +1,51 @@
+import { Info } from 'lucide-react';
+import TextArea from '../elements/TextArea';
+import { Tooltip } from 'react-tooltip';
+
+interface FunctionProps {
+	required?: boolean;
+	placeholder?: string;
+	id?: string;
+	info?: string;
+
+	label: string;
+}
+
+export default function TextAreaFormControl({
+	required = false,
+	label,
+	id,
+	info,
+	placeholder = '',
+}: FunctionProps) {
+	const tooltipId = `${label}-${id}`;
+
+	return (
+		<div className="my-3 flex flex-col items-start justify-start">
+			{info && (
+				<Tooltip id={tooltipId}>
+					<p>{info}</p>
+				</Tooltip>
+			)}
+			<label
+				className="flex items-center justify-start gap-x-1 text-sm font-semibold"
+				htmlFor={id}
+			>
+				<span>{required ? `${label}*` : label}</span>
+				{info && (
+					<span
+						data-tooltip-id={info && tooltipId}
+						data-tooltip-place="top"
+					>
+						<Info size={14} />
+					</span>
+				)}
+			</label>
+			<TextArea
+				placeholder={placeholder}
+				className="mt-0.5 w-lg py-2 placeholder:text-sm max-md:w-full"
+				id={id}
+			/>
+		</div>
+	);
+}
