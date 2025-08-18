c68c54cf1909a2744bb878f6b1aa68bf09a94211
sunil <sunil.shastry69@gmail.com>
2025-08-18 01:01:05 -0600

    Create template with boilerplate content for EditRulerTabsContent form section

---
+++ src/sections/issues/edit/EditRulerTabsContent.tsx
@@ -0,0 +1,12 @@
+import { TabsContent } from '@/components/ui/tabs';
+
+export default function EditRulerTabsContent() {
+	return (
+		<>
+			<TabsContent value="ruler">
+				<h2>Edit Ruler</h2>
+				<p>You can edit ruler data here</p>
+			</TabsContent>
+		</>
+	);
+}
