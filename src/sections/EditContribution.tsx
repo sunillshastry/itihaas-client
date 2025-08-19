adf70c2cd269cb8e37a3d2e1e48c62e74d997e51
sunil <sunil.shastry69@gmail.com>
2025-08-19 17:06:08 -0600

    Setup search param feature with edit contribution tabs with fixed logic

--- src/sections/EditContribution.tsx
+++ src/sections/EditContribution.tsx
@@ -5,12 +5,45 @@ import { Send } from 'lucide-react';
 import EditDynastyTabsContent from './issues/edit/EditDynastyTabsContent';
 import EditRulerTabsContent from './issues/edit/EditRulerTabsContent';
 import EditWarTabsContent from './issues/edit/EditWarTabsContent';
+import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+const DEFAULT_EDIT_CONTRIBUTION_TAB = 'dynasty';
 
 export default function EditContribution() {
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const tabType = searchParams.get('type') || DEFAULT_EDIT_CONTRIBUTION_TAB;
+
+	const [tabValue, setTabValue] = useState<string>(tabType);
+
+	function changeTab(e: string) {
+		setTabValue(e);
+
+		setSearchParams(function (currentParams) {
+			currentParams.set('type', e);
+			return currentParams;
+		});
+	}
+
+	useEffect(
+		function () {
+			setSearchParams(function (currentParams) {
+				currentParams.set('type', tabValue);
+				return currentParams;
+			});
+		},
+		[tabValue]
+	);
+
 	return (
 		<section className="from-primary-400 to-primary-700 text-primary-70 my-6 rounded-xl bg-linear-to-br p-8 max-md:px-4">
 			<div>
-				<Tabs defaultValue="dynasty">
+				<Tabs
+					defaultValue={tabValue}
+					value={tabValue}
+					onValueChange={changeTab}
+				>
 					<TabsList className="w-sm max-md:w-full">
 						<TabsTrigger value="dynasty">Dynasty</TabsTrigger>
 						<TabsTrigger value="ruler">Ruler</TabsTrigger>
