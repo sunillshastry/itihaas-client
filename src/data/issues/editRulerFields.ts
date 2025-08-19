e5ec1d4220edb5d8eecb1fae2f71839e61f42b23
sunil <sunil.shastry69@gmail.com>
2025-08-19 17:20:55 -0600

    Create a data source file with content for ruler edit contribution form fields

---
+++ src/data/issues/editRulerFields.ts
@@ -0,0 +1,61 @@
+import { IssuesFormControlOptions } from '@/interfaces/IssuesFormControlOptions';
+
+const editRulerFields: IssuesFormControlOptions[] = [
+	{
+		id: 1,
+		required: true,
+		label: 'Name',
+		placeholder: 'Name of the ruler you wish to edit',
+		type: 'input',
+		htmlId: 'edit-ruler-name',
+	},
+	{
+		id: 2,
+		required: true,
+		label: 'Link',
+		placeholder: 'Leave the ruler page link',
+		type: 'input',
+		htmlId: 'edit-ruler-link',
+		info: `Please make sure the provided link is not broken and correct; you can get the ruler page link using the 'Copy URL' button on its page`,
+	},
+	{
+		id: 3,
+		required: true,
+		label: 'Field Name',
+		placeholder:
+			'Only enter the name of the field that is incorrect or missing',
+		type: 'input',
+		htmlId: 'edit-ruler-field-name',
+		info: 'Only provide the name of the field that currently holds an empty value or has incorrect content',
+	},
+	{
+		id: 4,
+		required: true,
+		label: 'Field Content (Your response)',
+		placeholder: 'New or updated content to the field',
+		type: 'textarea',
+		htmlId: 'edit-ruler-field-content',
+		info: 'Provide the appropriate value (answer) for the previously mentioned field that holds incorrect or missing content',
+	},
+	{
+		id: 5,
+		required: true,
+		label: 'Source(s)',
+		placeholder: 'One or more sources backing your response',
+		type: 'textarea',
+		htmlId: 'edit-ruler-sources',
+		info: 'Include links to one or more sources that proves credibility about your response/answer to be factually correct',
+	},
+	{
+		id: 6,
+		htmlId: 'edit-ruler-comments',
+		type: 'textarea',
+		label: 'Additional Comments',
+		required: false,
+		placeholder:
+			'Include any additional comments that is closely relevant to this entity',
+		info: 'You can mention any new ideas and suggestions or provide any comments or context here',
+	},
+];
+
+export default editRulerFields;
