dd5377941857ef2b6ab6ca41f72da8dd87ffa419
sunil <sunil.shastry69@gmail.com>
2025-08-19 17:21:21 -0600

    Setup the actual form elements and render the result via data from editRulerFields data source file

--- src/sections/issues/edit/EditRulerTabsContent.tsx
+++ src/sections/issues/edit/EditRulerTabsContent.tsx
@@ -1,11 +1,48 @@
+import InputFormControl from '@/components/issues/InputFormControl';
+import TextAreaFormControl from '@/components/issues/TextAreaFormControl';
 import { TabsContent } from '@/components/ui/tabs';
+import editRulerFields from '@/data/issues/editRulerFields';
 
 export default function EditRulerTabsContent() {
 	return (
 		<>
 			<TabsContent value="ruler">
-				<h2>Edit Ruler</h2>
-				<p>You can edit ruler data here</p>
+				<div className="border-primary-70/35 border-b pb-2">
+					<h3 className="text-xl font-medium">Edit Ruler</h3>
+					<h6 className="mt-1 text-sm">
+						Please ensure all fields marked with an '*' are provided with
+						content. You can provide multiple entires per field by separating
+						them using a <span className="font-semibold underline">comma</span>.
+					</h6>
+				</div>
+
+				<div>
+					{editRulerFields.map(function (field) {
+						if (field.type === 'input') {
+							return (
+								<InputFormControl
+									key={field.id}
+									id={field.htmlId}
+									label={field.label}
+									placeholder={field.placeholder}
+									info={field.info}
+									required={field.required}
+								/>
+							);
+						} else if (field.type === 'textarea') {
+							return (
+								<TextAreaFormControl
+									key={field.id}
+									id={field.htmlId}
+									label={field.label}
+									placeholder={field.placeholder}
+									info={field.info}
+									required={field.required}
+								/>
+							);
+						}
+					})}
+				</div>
 			</TabsContent>
 		</>
 	);
