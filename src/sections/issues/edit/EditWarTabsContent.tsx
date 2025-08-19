674aa5e274f8362d2ace6a37494cebb03d6827d2
sunil <sunil.shastry69@gmail.com>
2025-08-19 17:27:11 -0600

    Use the data from editWarFields data source file to render output for edit war contribution form

--- src/sections/issues/edit/EditWarTabsContent.tsx
+++ src/sections/issues/edit/EditWarTabsContent.tsx
@@ -1,11 +1,48 @@
+import InputFormControl from '@/components/issues/InputFormControl';
+import TextAreaFormControl from '@/components/issues/TextAreaFormControl';
 import { TabsContent } from '@/components/ui/tabs';
+import editWarFields from '@/data/issues/editWarFields';
 
 export default function EditWarTabsContent() {
 	return (
 		<>
 			<TabsContent value="war">
-				<h2>Edit War</h2>
-				<p>You can edit war data here</p>
+				<div className="border-primary-70/35 border-b pb-2">
+					<h3 className="text-xl font-medium">Edit War</h3>
+					<h6 className="mt-1 text-sm">
+						Please ensure all fields marked with an '*' are provided with
+						content. You can provide multiple entires per field by separating
+						them using a <span className="font-semibold underline">comma</span>.
+					</h6>
+				</div>
+
+				<div>
+					{editWarFields.map(function (field) {
+						if (field.type === 'input') {
+							return (
+								<InputFormControl
+									key={field.id}
+									id={field.htmlId}
+									label={field.label}
+									placeholder={field.placeholder}
+									info={field.info}
+									required={field.required}
+								/>
+							);
+						} else if (field.type === 'textarea') {
+							return (
+								<TextAreaFormControl
+									key={field.id}
+									id={field.htmlId}
+									label={field.label}
+									placeholder={field.placeholder}
+									info={field.info}
+									required={field.required}
+								/>
+							);
+						}
+					})}
+				</div>
 			</TabsContent>
 		</>
 	);
