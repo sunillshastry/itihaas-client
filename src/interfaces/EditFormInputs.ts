89c287bab84baf80877c4c1c9b792c8584de96ad
sunil <sunil.shastry69@gmail.com>
2025-08-19 23:24:30 -0600

    Create an interface for react-hook-form for all edit contribution(s) fields

---
+++ src/interfaces/EditFormInputs.ts
@@ -0,0 +1,15 @@
+interface EditFormInputs {
+	name: string;
+	link: string;
+	fieldName: string;
+	fieldContent: string;
+	sources: string;
+	comments: string;
+
+	// User Info
+	userFullName: string;
+	userLocation: string;
+	userEmail: string;
+}
+
+export type { EditFormInputs };
