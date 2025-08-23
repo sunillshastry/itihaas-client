0d19478c5c0d61673d5796c4e242e32a7cc89b5d
sunil <sunil.shastry69@gmail.com>
2025-08-22 23:02:24 -0600

    Update home/ui 'offers' section by display random entity per section except war

--- src/ui/Offers.tsx
+++ src/ui/Offers.tsx
@@ -1,6 +1,35 @@
+import getRandomDynasty from '@/api/getRandomDynasty';
+import getRandomRuler from '@/api/getRandomRuler';
+import RandomEntityView from '@/components/views/RandomEntityView';
+import { Dynasty } from '@/interfaces/Dynasty';
+import { Ruler } from '@/interfaces/Ruler';
+import { useQuery } from '@tanstack/react-query';
+import { ArrowRight } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 function Offers() {
+	// Fetch random dynasty
+	const {
+		data: randomDynasty,
+		isPending: randomDynastyLoading,
+		refetch: refetchDynasty,
+	} = useQuery({
+		queryKey: ['random-dynasty'],
+		queryFn: getRandomDynasty,
+		refetchOnWindowFocus: false,
+	});
+
+	// Fetch random ruler
+	const {
+		data: randomRuler,
+		isPending: randomRulerLoading,
+		refetch: refetchRuler,
+	} = useQuery({
+		queryKey: ['random-ruler'],
+		queryFn: getRandomRuler,
+		refetchOnWindowFocus: false,
+	});
+
 	return (
 		<div className="text-primary-400 mt-6 max-sm:px-2">
 			<h2 className="border-primary-300/25 border-b text-xl font-semibold uppercase first-letter:text-3xl">
@@ -13,40 +42,86 @@ function Offers() {
 				</p>
 
 				<ul>
-					<li>
-						<span className="font-semibold underline">Dynasties</span>
-						<span className="ml-1">
-							Discover influential ruling houses from the Mauryas and Guptas to
-							the Marathas and Mughals. Learn about their origins, territories,
-							timelines, contributions, articles, and significance.&nbsp;
-						</span>
-						<span className="hover:text-primary-10 underline">
-							<Link to="/dynasties">View dynasties catalog</Link>
-						</span>
+					<li className="mt-2">
+						<p className="flex flex-col">
+							<span className="text-xl font-semibold underline">Dynasties</span>
+							<span className="mt-1">
+								Discover influential ruling houses from the Mauryas and Guptas
+								to the Marathas and Mughals. Learn about their origins,
+								territories, timelines, contributions, articles, and
+								significance. Here is a small snippet of what the entity content
+								might look like.
+							</span>
+						</p>
+
+						<RandomEntityView
+							type="dynasties"
+							entity={randomDynasty as Dynasty & Ruler}
+							loading={randomDynastyLoading}
+							refetch={refetchDynasty}
+						/>
+
+						<Link
+							to="/dynasties"
+							className="bg-primary-600 text-primary-60 hover:bg-primary-400 inline-flex items-center gap-x-2 rounded-sm p-3 text-base transition"
+						>
+							<span>Dynasties Catalog</span>
+							<span>
+								<ArrowRight size={17} />
+							</span>
+						</Link>
 					</li>
 
-					<li>
-						<span className="font-semibold underline">Rulers</span>
-						<span className="ml-1">
-							Delve into the lives and legacies of emperors, kings, and queens,
-							the leaders who shaped history with their governance, vision, and
-							conquests.&nbsp;
-						</span>
-						<span className="hover:text-primary-10 underline">
-							<Link to="/rulers">View rulers catalog</Link>
-						</span>
+					<li className="mt-10">
+						<p className="flex flex-col">
+							<span className="text-xl font-semibold underline">Rulers</span>
+							<span className="mt-1">
+								Delve into the lives and legacies of emperors, kings, and
+								queens, the leaders who shaped history with their governance,
+								vision, and conquests. Here is a small snippet of what the
+								entity content might look like.
+							</span>
+						</p>
+						<RandomEntityView
+							type="rulers"
+							entity={randomRuler as Dynasty & Ruler}
+							loading={randomRulerLoading}
+							refetch={refetchRuler}
+						/>
+
+						<Link
+							to="/rulers"
+							className="bg-primary-600 text-primary-60 hover:bg-primary-400 inline-flex items-center gap-x-2 rounded-sm p-3 text-base transition"
+						>
+							<span>Rulers Catalog</span>
+							<span>
+								<ArrowRight size={17} />
+							</span>
+						</Link>
 					</li>
 
-					<li>
-						<span className="font-semibold underline">Wars</span>
-						<span className="ml-1">
-							Explore pivotal battles and conflicts that redefined borders,
-							ideologies, and the course of history, from ancient wars to
-							colonial resistance.&nbsp;
-						</span>
-						<span className="hover:text-primary-10 underline">
-							<Link to="/wars">View wars catalog</Link>
-						</span>
+					<li className="mt-10">
+						<p className="flex flex-col">
+							<span className="text-xl font-semibold underline">Wars</span>
+							<span className="mt-1">
+								Explore pivotal battles and conflicts that redefined borders,
+								ideologies, and the course of history, from ancient wars to
+								colonial resistance. Here is a small snippet of what the entity
+								content might look like.
+							</span>
+						</p>
+						{/* TODO: Uncomment and setup request to Wars when implemented */}
+						{/* <RandomEntityView /> */}
+
+						<Link
+							to="/wars"
+							className="bg-primary-600 text-primary-60 hover:bg-primary-400 inline-flex items-center gap-x-2 rounded-sm p-3 text-base transition"
+						>
+							<span>Wars Catalog</span>
+							<span>
+								<ArrowRight size={17} />
+							</span>
+						</Link>
 					</li>
 				</ul>
 
