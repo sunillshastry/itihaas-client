4956b152650e9cddda0e81647017e15741d12461
sunil <sunil.shastry69@gmail.com>
2025-08-24 06:07:25 -0600

    Write up additional content within the API recovery section in the /register route for information about the 'resend' service

--- src/components/register/Recovery.tsx
+++ src/components/register/Recovery.tsx
@@ -4,7 +4,9 @@ function Recovery() {
 	return (
 		<>
 			<div className="mt-5">
-				<h4 className="text-primary-600 text-lg font-semibold">API Recovery</h4>
+				<h4 className="text-primary-600 text-lg font-semibold">
+					API Recovery and Verification
+				</h4>
 
 				<p>
 					If you have previously registered for an API key at Itihaas, and have
@@ -16,6 +18,20 @@ function Recovery() {
 					a couple of minutes and check your inbox for an email from Itihaas,
 					check your spam folder if you do not receive the recovery email in
 					under five minutes.
+				</p>
+
+				<p className="mt-1 font-medium">
+					On a related note, if you have also previously received an email with
+					a verification link, which has been expired now and you need a new
+					verification link without registering again, you can find support on
+					our{' '}
+					<Link
+						to="/api-resend"
+						className="text-primary-600 hover:text-primary-10 underline"
+					>
+						resend page
+					</Link>
+					.
 				</p>
 
 				<p className="mt-1 font-medium">
