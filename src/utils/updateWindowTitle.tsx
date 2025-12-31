d8638d39afefd689285593b4ef20302dd9e5987d
Sunil Shastry <sunil.shastry69@gmail.com>
2025-12-31 11:23:49 -0600

    Refactor all 'utils' source code files to include better documentation and licensing descriptions where necessary

--- src/utils/updateWindowTitle.tsx
+++ src/utils/updateWindowTitle.tsx
@@ -1,4 +1,15 @@
 /**
+ * Copyright (C) 2025 Itihaas | Sunil Shastry
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version
+ *
+ * To view full licensing and usage information, visit: https://github.com/sunillshastry/itihaas-api/blob/master/LICENSE
+ */
+
+/**
  * Set the title of the window document
  *
  * @param {function} setTitle A useState function to set the title state
